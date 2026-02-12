a76e5efc8dd49d89543eea3a88c03e4f1723920d
SAyC16 <143815233+SAyC16@users.noreply.github.com>
2026-02-12 16:34:08 +0200

    Added basic form handling and Typewriter pluging

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+    event.preventDefault();
 
+    new Typewriter("#poem", {
+      strings: "The poem will appear here",
+      autoStart: true,
+      delay: 1,
+      cursor:"",
+    });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
